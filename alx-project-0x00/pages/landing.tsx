069583ca2bbbca5c2c2b83e51128a653babf3de4
godwinLaryea <kalexgun@gmail.com>
2025-05-26 17:21:44 +0000

    Rendered buttons on landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
@@ -9,6 +10,9 @@ const Landing: React.FC = () => {
       <Card />
       <Card />
       <Card />
+      <Button title="Small" styles="text-sm rounded-sm bg-sky-200 p-2 mb-2 cursor-pointer" />
+      <Button title="Medium" styles="text-md rounded-md bg-sky-300 p-2 mb-2 cursor-pointer" />
+      <Button title="Large" styles="text-lg rounded-full bg-sky-400 px-4 py-1 cursor-pointer" />
     </>
   );
 };
