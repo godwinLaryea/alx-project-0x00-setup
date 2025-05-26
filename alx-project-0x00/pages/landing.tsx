a2daaf6c0360876f1e2a57b7a6a24e2ca17a055d
godwinLaryea <kalexgun@gmail.com>
2025-05-26 16:00:24 +0000

    minor fix

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -2,12 +2,14 @@ import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card/>
-      <Card/>
-      <Card/>
-    </div>
+    <>
+      <div>
+        <h1 className=" text-xl font-extralight">Landing Page</h1>
+      </div>
+      <Card />
+      <Card />
+      <Card />
+    </>
   );
 };
 
