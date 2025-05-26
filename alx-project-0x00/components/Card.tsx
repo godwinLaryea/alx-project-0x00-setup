65de4ebc56fa2c3b64715511e09c223ccfe02c2e
godwinLaryea <kalexgun@gmail.com>
2025-05-26 15:55:05 +0000

    fixed images file path

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import HOUSE_IMAGE from "@/public/assets/house.png";
-import STAR_IMAGE from "@/public/assets/star.png";
+import HOUSE_IMAGE from "@/public/assets/images/house.png";
+import STAR_IMAGE from "@/public/assets/images/star.png";
 import React from "react";
 import Pill from "./Pill";
 
 const Card: React.FC = () => {
   return (
-    <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
+    <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg">
       <Image
         className="rounded-lg"
         src={HOUSE_IMAGE}
