1e4ab4afbc21bedf9517a4a7892434673e445267
godwinLaryea <kalexgun@gmail.com>
2025-05-26 17:26:48 +0000

    creating multiple buttons and adjusting border radius of buttons

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,9 +10,18 @@ const Landing: React.FC = () => {
       <Card />
       <Card />
       <Card />
+      {/* Small Buttons */}
       <Button title="Small" styles="text-sm rounded-sm bg-sky-200 p-2 mb-2 cursor-pointer" />
+      <Button title="Small" styles="text-md rounded-md bg-sky-200 p-2 mb-2 cursor-pointer" />
+      <Button title="Small" styles="text-lg rounded-full bg-sky-200 p-2 mb-2 cursor-pointer" />
+      {/* Medium Buttons */}
+      <Button title="Medium" styles="text-sm rounded-sm bg-sky-300 p-2 mb-2 cursor-pointer" />
       <Button title="Medium" styles="text-md rounded-md bg-sky-300 p-2 mb-2 cursor-pointer" />
-      <Button title="Large" styles="text-lg rounded-full bg-sky-400 px-4 py-1 cursor-pointer" />
+      <Button title="Medium" styles="text-lg rounded-full bg-sky-300 p-2 mb-2 cursor-pointer" />
+      {/* Large Buttons */}
+      <Button title="Large" styles="text-sm rounded-sm bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
+      <Button title="Large" styles="text-md rounded-md bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
+      <Button title="Large" styles="text-lg rounded-full bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
     </>
   );
 };
