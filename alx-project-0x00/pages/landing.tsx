97b52bd1e72da77903cf4797908fbc6e99176d5e
godwinLaryea <kalexgun@gmail.com>
2025-05-26 17:28:58 +0000

    buttons update

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -14,14 +14,17 @@ const Landing: React.FC = () => {
       <Button title="Small" styles="text-sm rounded-sm bg-sky-200 p-2 mb-2 cursor-pointer" />
       <Button title="Small" styles="text-md rounded-md bg-sky-200 p-2 mb-2 cursor-pointer" />
       <Button title="Small" styles="text-lg rounded-lg bg-sky-200 p-2 mb-2 cursor-pointer" />
+      <Button title="Small" styles="text-lg rounded-full bg-sky-200 p-2 mb-2 cursor-pointer" />
       {/* Medium Buttons */}
       <Button title="Medium" styles="text-sm rounded-sm bg-sky-300 p-2 mb-2 cursor-pointer" />
       <Button title="Medium" styles="text-md rounded-md bg-sky-300 p-2 mb-2 cursor-pointer" />
       <Button title="Medium" styles="text-lg rounded-lg bg-sky-300 p-2 mb-2 cursor-pointer" />
+      <Button title="Medium" styles="text-lg rounded-full bg-sky-300 p-2 mb-2 cursor-pointer" />
       {/* Large Buttons */}
       <Button title="Large" styles="text-sm rounded-sm bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
       <Button title="Large" styles="text-md rounded-md bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
       <Button title="Large" styles="text-lg rounded-lg bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
+      <Button title="Large" styles="text-lg rounded-full bg-sky-400 px-4 py-1 mb-2 cursor-pointer" />
     </>
   );
 };
